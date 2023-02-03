1004bc2787fc35a36aa6458cba3d13c4388d10c1
GeorgeH <default_hyn@163.com>
2023-02-03 23:32:44 +0800

    docs:render

---
+++ vue3_cli_demo/src/10_render函数的使用/RenderCom.vue
@@ -0,0 +1,23 @@
+
+
+<script >
+import {h} from 'vue'
+    export default {
+        //在vue2.x中h函数是作为render函数的参数，vue3将其抽离了出来，所以需要import
+        render(){
+            /*
+            完整参数签名
+            function h(
+            type: string | Component,
+            props?: object | null,
+            children?: Children | Slot | Slots
+            ): VNode
+            */
+            return h('p',{class:'title'},'hello render')
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
