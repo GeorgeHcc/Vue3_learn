0af6d83acb49d8516cea34d857da7f68234491c2
GeorgeH <default_hyn@163.com>
2023-02-04 23:32:03 +0800

    feat:[global-diretion]

---
+++ vue3_cli_demo/src/11_自定义指令/GlobalDirection.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <span v-format-time>{{ 2234341421 }}</span>
+    </div>
+</template>
+
+<script>
+// import formatTime from './Directions/formatTime';
+    export default {
+        setup(){}
+    }
+</script>
+
+<style scoped>
+
+</style>
