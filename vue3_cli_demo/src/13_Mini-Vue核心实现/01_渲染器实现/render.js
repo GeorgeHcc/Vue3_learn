8da472d121022f2f00c571ab600495756d4a3f72
GeorgeH <default_hyn@163.com>
2023-02-06 02:02:30 +0800

    refactor:[Plugin]

--- vue3_cli_demo/src/13_Mini-Vue核心实现/01_渲染器实现/render.js
+++ vue3_cli_demo/src/13_Mini-Vue核心实现/01_渲染器实现/render.js
@@ -1,7 +1,33 @@
-export function h(tag,props,children){
-         return{
-            tag,
-            props,
-            children
+
+
+const h=(tag,props,children)=>{
+    //vNode->javascript 对象
+    return{
+        tag,
+        props,
+        children
+    }
+}
+const mount=(vNode,container)=>{
+    //创建真实DOM
+    const el=vNode.el=document.createElement(vNode.tag)
+   
+    //设置属性
+    if(vNode.props){
+        for(const key in vNode.props){
+            const value=vNode.props[key]
+            el.setAttribute(key,value)
         }
+    }
+    //
+    if(typeof vNode.children==='object'){
+        vNode.children.forEach((e)=>{
+            mount(e,el)
+        })
+    }else{
+        el.innerText=vNode.children
+    }
+    
+    container.appendChild(el)
+ 
 }
