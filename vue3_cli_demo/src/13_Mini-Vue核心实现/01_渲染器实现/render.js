d36d414cf803d5f08dd2a44705b65b34add14424
GeorgeH <default_hyn@163.com>
2023-02-11 00:41:39 +0800

    fix:[Mini-vue]

--- vue3_cli_demo/src/13_Mini-Vue核心实现/01_渲染器实现/render.js
+++ vue3_cli_demo/src/13_Mini-Vue核心实现/01_渲染器实现/render.js
@@ -37,11 +37,11 @@ const mount=(vNode,container)=>{
         vNode.children.forEach((e)=>{
             mount(e,el)
         })
-        container.appendChild(el)
+       
     }else{//没有子节点
         el.innerText=vNode.children
     }
-    
+     container.appendChild(el)
  
 }
 
