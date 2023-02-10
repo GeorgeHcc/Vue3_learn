f521874bb45cf3c22b43862cb7143f086429f77a
GeorgeH <default_hyn@163.com>
2023-02-10 20:28:27 +0800

    feat:[Mini-vue] reactive-vue2

--- vue3_cli_demo/src/13_Mini-Vue核心实现/02_响应式实现/reactive.js
+++ vue3_cli_demo/src/13_Mini-Vue核心实现/02_响应式实现/reactive.js
@@ -1,5 +1,5 @@
 
-//#region  Vue3响应式的实现
+//#region  Vue响应式的实现
 /**
  * 为什么Vue3使用proxy？
  * defineproperty劫持的是对象的属性，如果新增元素会再次调用defineproperty，
@@ -8,3 +8,92 @@
  */
 
 //#endregion
+
+class Dep{
+    constructor(){
+        this.subscribers=new Set()
+    }
+    addEffect(effect){
+        this.subscribers.add(effect)
+    }
+    depend(){
+        if(activeEffect){
+            this.addEffect(activeEffect)
+        }
+    }
+    notify(){
+        this.subscribers.forEach(effect=>{
+            effect()
+        })
+    }
+}
+let activeEffect=null;
+function watchEffect(effect){
+    // dep.addEffect(effect)
+    activeEffect=effect
+    effect()
+    activeEffect=null
+}
+/**
+ * Map({key:value}) key必须是字符串
+ * WeakMap({key:value})key是一个对象，弱引用
+ */
+let targetMap=new WeakMap()
+function getDep(target,key){
+    //1.根据target对象取出对应的Map对象
+    let depsMap=targetMap.get(target)
+    if(!depsMap){
+        depsMap=new Map();
+        targetMap.set(target,depsMap)
+    }
+    //2.取出具体的dep对象
+    let dep=depsMap.get(key)
+    if(!dep){
+        dep=new Dep();
+        depsMap.set(key,dep);
+    }
+    return dep;
+
+}
+//vue2对raw进行数据劫持
+function reactive(raw){
+    Object.keys(raw).forEach(key=>{
+        const dep=getDep(raw,key)
+        let value=raw[key]
+        Object.defineProperty(raw,key,{
+            get(){
+                dep.depend()
+                return value
+            },
+            set(newVal){
+               if(value!==newVal){
+                value=newVal;
+                dep.notify();
+               }
+            }
+        })
+
+    })
+    return raw;
+}
+
+//测试代码
+// const dep=new Dep()
+const info=reactive({counter:100,name:'georgeH'});
+const foo=reactive({height:300})
+
+watchEffect(function doubleCounter(){
+    console.log('effect1:',info.counter*2);
+})
+
+watchEffect(function powerCounter(){
+    console.log('effect2:',info.counter**2);
+})
+
+watchEffect(function powerCounter(){
+    console.log('effect3:',foo.height);
+})
+
+info.counter++;
+info.name='kobe'
+foo.height=100
